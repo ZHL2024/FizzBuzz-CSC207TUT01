9ff3f2ffe906311099d00909e3f79c0ab07e8cfd
Zihan Lu <wilsonlzh@foxmail.com>
2025-09-08 19:56:36 -0400

    add func multiples

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,11 +2,15 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+    }
+    public static int multiples(int n, int a, int b) {
         int i = 1;
-        for (i = 1; i <= 1000; i++) {
-            if (i % 3 == 0 || i % 5 == 0) {
-                System.out.println(i);
+        int j = 0;
+        for (i = 1; i <= n; i++) {
+            if (i % a == 0 || i % b == 0) {
+                j++;
             }
         }
+        return j;
     }
 }
