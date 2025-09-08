39613214dbffecee8b3382f6f0889aac89bad940
Zihan Lu <wilsonlzh@foxmail.com>
2025-09-08 18:55:15 -0400

    Add the main func to find needed nums.

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,12 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+        int i = 1;
+        for (i = 1; i <= 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
